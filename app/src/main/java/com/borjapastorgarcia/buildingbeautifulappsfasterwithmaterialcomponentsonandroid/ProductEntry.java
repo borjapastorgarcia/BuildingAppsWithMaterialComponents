e78f3db13a4a2a185641266546a8e7fd6dd3d8ee
Borja Pastor Garcia <borjapg616@gmail.com>
2018-04-28 15:32:46 +0200

    Second commit

---
+++ app/src/main/java/com/borjapastorgarcia/buildingbeautifulappsfasterwithmaterialcomponentsonandroid/ProductEntry.java
@@ -0,0 +1,15 @@
+package com.borjapastorgarcia.buildingbeautifulappsfasterwithmaterialcomponentsonandroid;
+
+public class ProductEntry {
+    public final String title;
+    public final String url;
+    public final String price;
+    public final String description;
+
+    public ProductEntry(String title, String url, String price, String description) {
+        this.title = title;
+        this.url = url;
+        this.price = price;
+        this.description = description;
+    }
+}
